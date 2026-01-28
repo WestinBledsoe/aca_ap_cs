73a915a9f46205f218b32c1a3c50be833c97e96f
Westin <westin.bledsoe@student.atlantaclassical.org>
2026-01-28 14:43:42 -0500

    local

---
+++ algos.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void print_0_10(){
+    for (int i=0;i<=10;i++)
+    {
+        printf("%d \n",i);
+    }
+}
+void print_2_20(){
+    for (int g=2;g<=20;g++)
+    {
+        printf("%d \n",g);
+    }
+}
+void print_4_40(){
+    for (int h=4;h<=40;[2])
+    {
+        printf("%d \n",h);
+    }
+}
+int main(){
+    print_0_10();
+    print_2_20();
+    print_4_40();
+}
