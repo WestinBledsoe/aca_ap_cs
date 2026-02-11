9f63740da1bc931f924418cd873006c7f7de0285
Westin <westin.bledsoe@student.atlantaclassical.org>
2026-02-11 15:21:17 -0500

    brithday

---
+++ test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main(){
+
+int array [85];
+for (int l=0; l<85; l++){
+    array [i] = i;
+    printf ("%d /n", array [l]);
+}
+
+int num [20][20];
+for (int i=0; i<20; i++){
+    for (int j=0; j<20; j++); 
+}
+
+}
